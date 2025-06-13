718e7f641076ce6b49b74caad2ea8ce6f2089099
Tarun Sharma <tarunshrm768@gmail.com>
2025-06-13 16:56:54 +0530

    add more categories in dictation schema commit

--- models/Dictation.js
+++ models/Dictation.js
@@ -1,29 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const dictationSchema = new mongoose.Schema({
-//   fileupload: {
-//     type: mongoose.Schema.Types.ObjectId, // GridFS file id
-//     required: true,
-//   },
-//   paragraphText: {
-//     type: String,
-//     required: true,
-//   },
-//   totalwords: {
-//     type: Number,
-//     required: true,
-//   },
-//   speed: {
-//     type: Number,
-//     required: true,
-//   },
-// }, {
-//   timestamps: true,
-// });
-
-// // module.exports = mongoose.model('Dictation', dictationSchema);
-// module.exports = mongoose.model('Dictation', dictationSchema, 'steno');
-
 const mongoose = require('mongoose');
 
 const dictationSchema = new mongoose.Schema({
@@ -37,7 +11,7 @@ const dictationSchema = new mongoose.Schema({
   },
   category: {
     type: String,
-    enum: ['Court', 'SSC', 'Railway', 'Other'],
+    enum: ['Court', 'SSC', 'Railway', 'KC', 'Progressive', 'Medical', 'AIIMS','Other'],
     default: 'Other',
     required: true,
   },
